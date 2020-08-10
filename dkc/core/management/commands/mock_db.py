f8450c2589385a1cb839403ea9f925cc07c1f50d
Zach Mullen <zach.mullen@kitware.com>
2020-08-10 11:11:59 -0400

    Recursive fake folder tree generation command

--- dkc/core/management/commands/mock_db.py
+++ dkc/core/management/commands/mock_db.py
@@ -8,12 +8,19 @@ from dkc.core.models.folder import Folder
 fake = Faker()
 
 
+def _populate_subtree(folder: Folder, depth: int, branching: int) -> None:
+    if depth == 0:
+        return
+
+    for _ in range(branching):
+        child = folder.add_child(name=fake.word())
+        _populate_subtree(child, depth - 1, branching)
+
+
 @click.command()
 @click.argument('depth', type=click.INT)
 @click.argument('branching', type=click.INT)
-def command(depth, branching):
-    for _root_num in range(10):
+def command(depth: int, branching: int):
+    for _ in range(branching):
         root_folder: Folder = Folder.add_root(name=fake.word())
-
-        for _subfolder_num in range(5):
-            root_folder.add_child(name=fake.word())
+        _populate_subtree(root_folder, depth - 1, branching)
