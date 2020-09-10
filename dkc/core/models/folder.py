fb6475773324047cc0412483552dc63dc4347fa3
Zach Mullen <zach.mullen@kitware.com>
2020-09-10 14:06:32 -0400

    Cache depth field on folders

--- dkc/core/models/folder.py
+++ dkc/core/models/folder.py
@@ -4,6 +4,7 @@ from typing import List
 
 from django.core import validators
 from django.db import models
+from django.dispatch import receiver
 from django_extensions.db.models import TimeStampedModel
 
 from dkc.core.exceptions import MaxFolderDepthExceeded
@@ -23,6 +24,13 @@ class Folder(TimeStampedModel, models.Model):
         ],
     )
 
+    depth = models.PositiveSmallIntegerField(
+        validators=[
+            validators.MaxValueValidator(MAX_TREE_HEIGHT, message='Maximum folder depth exceeded.'),
+        ],
+        editable=False,
+    )
+
     # TODO: What max_length?
     description = models.TextField(max_length=3000)
 
@@ -30,6 +38,11 @@ class Folder(TimeStampedModel, models.Model):
     # owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+
+    @property
+    def child_folders(self):
+        """Provide a QuerySet of the child folders of this folder."""
+        return Folder.objects.filter(parent=self)
 
     # # Prevent deletion of quotas while a folder references them
     # quota = models.ForeignKey(Quota, on_delete=models.PROTECT)
@@ -46,3 +59,14 @@ class Folder(TimeStampedModel, models.Model):
                 raise MaxFolderDepthExceeded()
 
         return path[::-1]
+
+
+@receiver(models.signals.pre_save, sender=Folder)
+def _folder_pre_save(sender, instance, *args, **kwargs):
+    if instance.depth is None:
+        if instance.parent is None:
+            instance.depth = 0
+        else:
+            instance.depth = instance.parent.depth + 1
+            if instance.depth > Folder.MAX_TREE_HEIGHT:
+                raise MaxFolderDepthExceeded()
--- dkc/core/tests/test_folder.py
+++ dkc/core/tests/test_folder.py
@@ -1,6 +1,16 @@
 from django.core.exceptions import ValidationError
 import pytest
 
+from dkc.core.exceptions import MaxFolderDepthExceeded
+from dkc.core.models.folder import Folder
+
+
+@pytest.fixture
+def limited_tree_height():
+    original, Folder.MAX_TREE_HEIGHT = Folder.MAX_TREE_HEIGHT, 3
+    yield Folder.MAX_TREE_HEIGHT
+    Folder.MAX_TREE_HEIGHT = original
+
 
 def test_folder_name_invalid(folder_factory):
     folder = folder_factory.build(name='name / withslash')
@@ -9,3 +19,23 @@ def test_folder_name_invalid(folder_factory):
     # so it's critical to match the error by string content
     with pytest.raises(ValidationError, match='Name may not contain forward slashes'):
         folder.full_clean()
+
+
+@pytest.mark.django_db
+def test_root_folder_depth_is_zero(folder):
+    assert folder.depth == 0
+
+
+@pytest.mark.django_db
+def test_child_folder_depth_computed(folder, folder_factory):
+    child = folder_factory(parent=folder)
+    assert child.depth == folder.depth + 1
+
+
+@pytest.mark.django_db
+def test_folder_max_depth_enforced(folder, folder_factory, limited_tree_height):
+    for _ in range(limited_tree_height):
+        folder = folder_factory(parent=folder)
+
+    with pytest.raises(MaxFolderDepthExceeded):
+        folder_factory(parent=folder)
