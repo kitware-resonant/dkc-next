d3628f66f67aca4a6f99d9196dfc47c19f772c6f
Zach Mullen <zach.mullen@kitware.com>
2020-09-10 10:59:41 -0400

    Move API under /v2
    This will allow us to place in a /v1 compatibility layer for current DKC
    if we wanted to.

--- dkc/core/tests/test_file.py
+++ dkc/core/tests/test_file.py
@@ -10,7 +10,7 @@ def test_file_checksum(file_factory):
 
 @pytest.mark.django_db
 def test_file_rest_retrieve(api_client, file):
-    resp = api_client.get(f'/api/v1/files/{file.id}')
+    resp = api_client.get(f'/api/v2/files/{file.id}')
     assert resp.status_code == 200
     # Inspect .data to avoid parsing the response content
     assert resp.data['name'] == file.name
