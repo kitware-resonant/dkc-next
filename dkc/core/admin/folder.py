baa6d2fbfc056c073cb6fcd640522630b32b71c4
Zach Mullen <zach.mullen@kitware.com>
2020-09-30 12:59:43 -0400

    Allow files to be added via django admin

--- dkc/core/admin/folder.py
+++ dkc/core/admin/folder.py
@@ -6,3 +6,6 @@ from dkc.core.models import Folder
 @admin.register(Folder)
 class FolderAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'parent']
+    list_display_links = ['id', 'name']
+
+    search_fields = ['name', 'id']
