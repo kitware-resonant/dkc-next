d20fb049508b27713126aaeb7a7a3ef6246568fc
Zach Mullen <zach.mullen@kitware.com>
2020-10-12 20:04:02 -0400

    Don't create new users with mock_db script

--- dkc/core/management/commands/mock_db.py
+++ dkc/core/management/commands/mock_db.py
@@ -1,5 +1,6 @@
 import typing
 
+from django.contrib.auth.models import User
 import djclick as click
 
 from dkc.core.models.folder import Folder
@@ -7,16 +8,16 @@ from dkc.core.tests.factories import FileFactory, FolderFactory
 
 
 def _populate_subtree(
-    folder: typing.Optional[Folder], depth: int, branching: int, files: int
+    folder: typing.Optional[Folder], depth: int, branching: int, files: int, user: User
 ) -> None:
     if depth == 0:
         for _ in range(files):
-            FileFactory(folder=folder, creator=None)
+            FileFactory(folder=folder, creator=user)
         return
 
     for _ in range(branching):
-        child: Folder = FolderFactory(parent=folder)
-        _populate_subtree(child, depth - 1, branching, files)
+        child: Folder = FolderFactory(parent=folder, owner=user)
+        _populate_subtree(child, depth - 1, branching, files, user)
 
 
 @click.command()
@@ -24,4 +25,5 @@ def _populate_subtree(
 @click.argument('branching', type=click.INT)
 @click.argument('files', type=click.INT, default=0)
 def command(depth: int, branching: int, files: int):
-    _populate_subtree(None, depth, branching, files)
+    user = User.objects.first()
+    _populate_subtree(None, depth, branching, files, user)
