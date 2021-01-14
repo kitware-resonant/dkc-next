922787a075b5b04a6947dc89fe8eba2ca7bc0a3d
Zach Mullen <zach.mullen@kitware.com>
2021-01-14 10:49:40 -0500

    Use copy of instance for partial update validation

--- dkc/core/rest/utils.py
+++ dkc/core/rest/utils.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from django.core.exceptions import ValidationError as DjangoValidationError
 from rest_framework import serializers
 
@@ -5,10 +7,11 @@ from rest_framework import serializers
 class FullCleanModelSerializer(serializers.ModelSerializer):
     def validate(self, data):
         if self.partial:  # partial update
+            instance = deepcopy(self.instance)
             for k, v in data.items():
-                setattr(self.instance, k, v)
+                setattr(instance, k, v)
             try:
-                self.instance.full_clean()
+                instance.full_clean()
             except DjangoValidationError as exc:
                 raise serializers.ValidationError(serializers.as_serializer_error(exc))
         else:  # create or full update
