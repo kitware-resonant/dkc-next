37ed172fcd3b88df269838f06e7095554bceea45
Brian Helba <brian.helba@kitware.com>
2021-01-30 22:10:05 -0500

    Move file REST tests to their own file

--- dkc/core/tests/test_file.py
+++ dkc/core/tests/test_file.py
@@ -11,14 +11,6 @@ def test_file_checksum(file_factory):
 
 
 @pytest.mark.django_db
-def test_file_rest_retrieve(admin_api_client, file):
-    resp = admin_api_client.get(f'/api/v2/files/{file.id}')
-    assert resp.status_code == 200
-    # Inspect .data to avoid parsing the response content
-    assert resp.data['name'] == file.name
-
-
-@pytest.mark.django_db
 def test_file_sibling_names_unique(file, file_factory):
     sibling = file_factory.build(folder=file.folder, name=file.name, creator=None)
     with pytest.raises(IntegrityError, match=r'Key .* already exists\.'):
@@ -38,12 +30,3 @@ def test_file_sibling_names_unique_folders(folder, folder_factory, file_factory)
 @pytest.mark.django_db
 def test_file_size_computed(file):
     assert file.size == file.blob.size
-
-
-@pytest.mark.django_db
-def test_file_list_default_ordering(admin_api_client, folder, file_factory):
-    for name in ('B', 'C', 'A'):
-        file_factory(name=name, folder=folder)
-    resp = admin_api_client.get('/api/v2/files', data={'folder': folder.id})
-    assert resp.status_code == 200
-    assert [f['name'] for f in resp.data['results']] == ['A', 'B', 'C']
