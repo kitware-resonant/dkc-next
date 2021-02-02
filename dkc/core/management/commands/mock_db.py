47df4a1c22fbac91a28ede807ae4d1b43c3935b1
Brian Helba <brian.helba@kitware.com>
2021-02-02 15:08:56 -0500

    Fix mock_db

--- dkc/core/management/commands/mock_db.py
+++ dkc/core/management/commands/mock_db.py
@@ -1,27 +1,34 @@
 import typing
 
+from django.contrib.auth.models import User
 import djclick as click
 
+from dkc.core.models import Tree
 from dkc.core.models.folder import Folder
+from dkc.core.permissions import Permission, PermissionGrant
 from dkc.core.tests.factories import FileFactory, FolderFactory
 
 
 def _populate_subtree(
-    folder: typing.Optional[Folder], depth: int, branching: int, files: int
+    folder: typing.Optional[Folder], depth: int, user: User, branching: int, files: int
 ) -> None:
     if depth == 0:
         for _ in range(files):
-            FileFactory(folder=folder, creator=None)
+            FileFactory(folder=folder, creator=user)
         return
 
     for _ in range(branching):
-        child: Folder = FolderFactory(parent=folder)
-        _populate_subtree(child, depth - 1, branching, files)
+        child: Folder = FolderFactory(parent=folder, creator=user)
+        _populate_subtree(child, depth - 1, user, branching, files)
 
 
 @click.command()
 @click.argument('depth', type=click.INT)
 @click.argument('branching', type=click.INT)
 @click.argument('files', type=click.INT, default=0)
-def command(depth: int, branching: int, files: int):
-    _populate_subtree(None, depth, branching, files)
+@click.argument('user_id', type=click.INT, default=None)
+def command(depth: int, branching: int, files: int, user_id: int):
+    user = User.objects.get(id=user_id) if user_id else User.objects.first()
+    _populate_subtree(None, depth, user, branching, files)
+    for tree in Tree.objects.all():
+        tree.grant_permission(PermissionGrant(user_or_group=user, permission=Permission.admin))
