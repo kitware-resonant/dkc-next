5b368eef61cf82e31c252bee228e3100241e4b67
Brian Helba <brian.helba@kitware.com>
2021-02-02 17:09:52 -0500

    Improve argument handling

--- dkc/core/management/commands/mock_db.py
+++ dkc/core/management/commands/mock_db.py
@@ -6,7 +6,7 @@ import djclick as click
 from dkc.core.models import Tree
 from dkc.core.models.folder import Folder
 from dkc.core.permissions import Permission, PermissionGrant
-from dkc.core.tests.factories import FileFactory, FolderFactory
+from dkc.core.tests.factories import FileFactory, FolderFactory, UserFactory
 
 
 def _populate_subtree(
@@ -23,12 +23,12 @@ def _populate_subtree(
 
 
 @click.command()
-@click.argument('depth', type=click.INT)
-@click.argument('branching', type=click.INT)
-@click.argument('files', type=click.INT, default=0)
-@click.argument('user_id', type=click.INT, default=None)
-def command(depth: int, branching: int, files: int, user_id: int):
-    user = User.objects.get(id=user_id) if user_id else User.objects.first()
+@click.argument('depth', type=click.INT, default=3)
+@click.argument('branching', type=click.INT, default=3)
+@click.argument('files', type=click.INT, default=3)
+@click.option('--user-id', type=click.INT)
+def command(depth: int, branching: int, files: int, user_id: typing.Optional[int]):
+    user = User.objects.get(id=user_id) if user_id is not None else UserFactory()
     _populate_subtree(None, depth, user, branching, files)
     for tree in Tree.objects.all():
         tree.grant_permission(PermissionGrant(user_or_group=user, permission=Permission.admin))
