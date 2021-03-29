fd811f6a4302ff950e3e50fd90eb3180f14c4a67
Brian Helba <brian.helba@kitware.com>
2021-03-29 13:31:27 -0400

    Use a Django ORM query for trees_by_file_count_view

--- dkc/core/views.py
+++ dkc/core/views.py
@@ -1,7 +1,9 @@
 from django.contrib.admin.views.decorators import staff_member_required
-from django.db import connection
+from django.db.models import Count, F
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from dkc.core.models import Tree
 
 
 @staff_member_required
@@ -9,21 +11,15 @@ def staff_home(request: HttpRequest) -> HttpResponse:
     return render(request, 'core/staff_home.html')
 
 
-TREES_BY_FILE_COUNT_Q = """
-SELECT folder.id, folder.name, sq.c as file_count, pg_size_pretty(folder.size) as size
-FROM core_folder as folder, (
-  SELECT tree.id as tree_id, count(*) as c
-  FROM core_file as file, core_folder as folder, core_tree as tree
-  WHERE file.folder_id = folder.id
-  AND folder.tree_id = tree.id
-  GROUP BY tree.id
-) as sq WHERE folder.tree_id = sq.tree_id AND folder.parent_id IS NULL ORDER BY sq.c DESC
-"""
-
-
 @staff_member_required
 def trees_by_file_count_view(request: HttpRequest) -> HttpResponse:
-    with connection.cursor() as cursor:
-        cursor.execute(TREES_BY_FILE_COUNT_Q)
-        rows = cursor.fetchall()
-    return render(request, 'core/trees_by_file_count.html', {'rows': rows})
+    trees_annotated = (
+        Tree.objects.annotate(
+            size=F('quota__used'),
+            num_files=Count('all_folders__files'),
+        )
+        .filter(all_folders__parent__isnull=True)
+        .annotate(name=F('all_folders__name'))
+        .order_by('-num_files')
+    )
+    return render(request, 'core/trees_by_file_count.html', {'trees': trees_annotated})
