3b940cc5b4bbd312519878c445dba869f6f7b8eb
Brian Helba <brian.helba@kitware.com>
2021-04-09 11:31:32 -0400

    Fix migration ordering

--- dkc/core/migrations/0011_authorizedupload.py
+++ dkc/core/migrations/0011_authorizedupload.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('core', '0009_file_pre_creation'),
+        ('core', '0010_legacy_ids'),
     ]
 
     operations = [
